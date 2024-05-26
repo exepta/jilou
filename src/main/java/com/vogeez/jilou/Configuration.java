01f86ff8302821cb55ecb8b2ef1542f692bad785
Exepta <genshin.exepta@outlook.com>
2024-05-26 13:39:53 +0200

    feat: include function for fetch version from gradle.properties

--- src/main/java/com/vogeez/jilou/Configuration.java
+++ src/main/java/com/vogeez/jilou/Configuration.java
@@ -1,6 +1,16 @@
 package com.vogeez.jilou;
 
+import org.lwjgl.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public final class Configuration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Configuration.class);
 
     public static void addFromArguments(String[] args) {
         fetchFromArguments(args, false);
@@ -12,6 +22,17 @@ public final class Configuration {
 
     public static void fetchFromArguments(String[] args, boolean override) {
 
+    }
+
+    public static void printVersion() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("gradle.properties"));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        LOG.info("JILOU Version {}", properties.getProperty("version"));
+        LOG.info("LWJGL Version {}", Version.getVersion().substring(0, 5));
     }
 
 }
