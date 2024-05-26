28c6eaa17646d9bce6ef2bffe9593bc1871d63e6
Exepta <genshin.exepta@outlook.com>
2024-05-26 13:40:29 +0200

    test: include class with main function for testing the ui framework.

---
+++ src/main/java/com/vogeez/jilou/example/FirstWindowExample.java
@@ -0,0 +1,11 @@
+package com.vogeez.jilou.example;
+
+import com.vogeez.jilou.UIApplication;
+
+public class FirstWindowExample {
+
+    public static void main(String[] args) {
+        UIApplication.load(args);
+    }
+
+}
