0513aba9f35c3cdba03eb8b96bbee796c290cc25
Exepta <genshin.exepta@outlook.com>
2024-05-26 22:13:31 +0200

    feat: update example class for display the first window.

--- src/main/java/com/vogeez/jilou/example/FirstWindowExample.java
+++ src/main/java/com/vogeez/jilou/example/FirstWindowExample.java
@@ -1,11 +1,14 @@
 package com.vogeez.jilou.example;
 
 import com.vogeez.jilou.UIApplication;
+import com.vogeez.jilou.ui.Window;
 
 public class FirstWindowExample {
 
     public static void main(String[] args) {
         UIApplication.load(args);
+
+        Window window = new Window();
     }
 
 }
