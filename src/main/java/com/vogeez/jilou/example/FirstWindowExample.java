71539b01c8ba84cb73b369cff9893d50a7601957
exepta <genshin.exepta@outlook.com>
2024-05-27 13:01:30 +0200

    fix: include setBackground.

--- src/main/java/com/vogeez/jilou/example/FirstWindowExample.java
+++ src/main/java/com/vogeez/jilou/example/FirstWindowExample.java
@@ -2,14 +2,15 @@ package com.vogeez.jilou.example;
 
 import com.vogeez.jilou.ApplicationFactory;
 import com.vogeez.jilou.UIApplication;
+import com.vogeez.jilou.style.Color;
 import com.vogeez.jilou.ui.Window;
 
 public class FirstWindowExample {
 
     public static void main(String[] args) {
         UIApplication.load(args);
-
         Window window = ApplicationFactory.createWindow(null, "Test", Window.class);
+        window.setBackground(Color.SEAGREEN);
     }
 
 }
