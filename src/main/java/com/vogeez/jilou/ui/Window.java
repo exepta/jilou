fe3e0c06db5f7e606e47e6956297b065006f5109
exepta <genshin.exepta@outlook.com>
2024-05-27 10:28:27 +0200

    fix: added constructors for handling at ApplicationFactory.java

--- src/main/java/com/vogeez/jilou/ui/Window.java
+++ src/main/java/com/vogeez/jilou/ui/Window.java
@@ -2,6 +2,14 @@ package com.vogeez.jilou.ui;
 
 public class Window extends AbstractUIContainedWindow {
 
+    public Window() {
+        this(null);
+    }
+
+    public Window(String uid) {
+        super(uid);
+    }
+
     @Override
     public void destroy() {
 
