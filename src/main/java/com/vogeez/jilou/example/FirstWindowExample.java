884be525bcbba7b2e8b137c4f2e7ccff4dc54587
Exepta <genshin.exepta@outlook.com>
2024-05-27 20:01:19 +0200

    fix: include child rectangle test

--- src/main/java/com/vogeez/jilou/example/FirstWindowExample.java
+++ src/main/java/com/vogeez/jilou/example/FirstWindowExample.java
@@ -4,13 +4,17 @@ import com.vogeez.jilou.ApplicationFactory;
 import com.vogeez.jilou.UIApplication;
 import com.vogeez.jilou.style.Color;
 import com.vogeez.jilou.ui.Window;
+import com.vogeez.jilou.ui.widgets.shapes.Rectangle;
 
 public class FirstWindowExample {
 
     public static void main(String[] args) {
         UIApplication.load(args);
         Window window = ApplicationFactory.createWindow(null, "Test", Window.class);
-        window.setBackground(Color.CORAL);
+        window.setBackground(Color.randomRGB());
+
+        Rectangle rectangle = new Rectangle();
+        window.getScene().addChild(rectangle);
     }
 
 }
